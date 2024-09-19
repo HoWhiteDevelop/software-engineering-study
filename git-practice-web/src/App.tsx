58e540bb646a6eccc33e2e49e39828a9be0bc2da
AmizukiZ <3073877601@qq.com>
2024-09-19 21:29:07 +0800

    代码格式化

--- git-practice-web/src/App.tsx
+++ git-practice-web/src/App.tsx
@@ -1,43 +1,41 @@
-import './App.css'
-import {faker} from "@faker-js/faker"
-import RestartButton from './components/RestartButton'
-import Result from './components/Result'
-import UserTypings from './components/UserTypings'
+import "./App.css";
+import { faker } from "@faker-js/faker";
+import RestartButton from "./components/RestartButton";
+import Result from "./components/Result";
+import UserTypings from "./components/UserTypings";
 
-const words = faker.word.words(15)
+const words = faker.word.words(15);
 
 function App() {
-
   // text-primary-500
   return (
-  <div>
-    <TimeLeftReminder timeLeft={20}/>
-    <GenerateRandom words={words}/> 
-    <UserTypings userInfo={"he llo"} className={"flex flex-row items-center text-primary-400 mt-10"}/>
-    <RestartButton 
-    className={"m-auto text-slate-500"} 
-    onRestart = {()=>{null}}/>
-    <Result 
-    errors={90}
-    accuracyPercentage={75}
-    total={100}
-    className={"mt-10"}/>
-   
-  </div>
-)}
+    <div>
+      <TimeLeftReminder timeLeft={20} />
+      <GenerateRandom words={words} />
+      <UserTypings
+        userInfo={"he llo"}
+        className={"flex flex-row items-center text-primary-400 mt-10"}
+      />
+      <RestartButton
+        className={"m-auto text-slate-500"}
+        onRestart={() => {
+          null;
+        }}
+      />
+      <Result
+        errors={90}
+        accuracyPercentage={75}
+        total={100}
+        className={"mt-10"}
+      />
+    </div>
+  );
+}
 
-const GenerateRandom = ({words}:{words:string})=>{
-  return(
-    <div className=' text-slate-500 text-4xl'>
-      words:{words}
-    </div>
-  )
-}
-const TimeLeftReminder = ({timeLeft}:{timeLeft:number})=>{
-  return(
-    <div className=' text-primary-500 text-xl'>
-        time:{timeLeft}
-    </div>
-  )
-}
-export default App
+const GenerateRandom = ({ words }: { words: string }) => {
+  return <div className=" text-slate-500 text-4xl">words:{words}</div>;
+};
+const TimeLeftReminder = ({ timeLeft }: { timeLeft: number }) => {
+  return <div className=" text-primary-500 text-xl">time:{timeLeft}</div>;
+};
+export default App;
--- git-practice-web/src/components/UserTypings.tsx
+++ git-practice-web/src/components/UserTypings.tsx
@@ -1,20 +1,19 @@
 const UserTypings = ({
-    userInfo,
-    className
-}:{
-    userInfo:string,
-    className?:string
-})=>{
-    const typeCharacter = userInfo.replace(/ /g,'\u00A0').split('')
-    console.log(typeCharacter)
-    return(
-        <div className={className}>
-            {typeCharacter.map((item, index) => (
-                <span key={index}>{item}</span>
-            ))} 
-            
-        </div>
-    )
-}
+  userInfo,
+  className,
+}: {
+  userInfo: string;
+  className?: string;
+}) => {
+  const typeCharacter = userInfo.replace(/ /g, "\u00A0").split("");
+  console.log(typeCharacter);
+  return (
+    <div className={className}>
+      {typeCharacter.map((item, index) => (
+        <span key={index}>{item}</span>
+      ))}
+    </div>
+  );
+};
 
-export default UserTypings
+export default UserTypings;
